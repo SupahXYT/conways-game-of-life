0ff70b1838b110315754901b32abe550ddeee82a
SupahXYT <josuefirelol@gmail.com>
2021-09-17 11:05:25 -0700

    leaev

--- Game.py
+++ Game.py
@@ -1,37 +1,77 @@
 
 def neighbors(grid, row, col):
     neighbors = 0
+
+    if(row != 0 and row < len(grid) - 2 and col != 0 and col < 39):
+
+        if(grid[row+1][col]):
+            print("row+1 col")
+            neighbors += 1
+        if(grid[row-1][col]):
+            print("row-1 col")
+            neighbors += 1
+        if(grid[row][col+1]):
+            print("row col+1")
+            neighbors += 1
+        if(grid[row][col-1]):
+            print("row col-1")
+            neighbors += 1
+
+        ## Check corners 
+        if(grid[row+1][col+1]):
+            print("row+1 col+1")
+            neighbors += 1
+        if(grid[row+1][col-1]):
+            print("row+1 col-1")
+            neighbors += 1
+        if(grid[row-1][col+1]):
+            print("row-1 col+1")
+            neighbors += 1
+        if(grid[row-1][col-1]):
+            print("row-1 col-1")
+            neighbors += 1
+
+    if(neighbors > 0):
+        print(f'neighbor {neighbors}')
+
     return neighbors
 
 class Game:
 
     def __init__(self, width, height):
-        self.grid = [[]]
         self.steps = 0
-
-        for i in range(0, width):
-            for j in range(0, height):
-                self.grid[i][j].append(False)
+        self.grid = [[False for i in range(height)] for j in range(width)]
+        self.grid[3][6] = True
+        self.grid[4][6] = True
+        self.grid[5][6] = True
+        print(len(self.grid[0]))
 
     def act(self, snapshot, row, col):
         if(snapshot[row][col]): # if alive
             if(neighbors(snapshot, row, col) < 2):
+                print("die")
                 self.grid[row][col] = False
             elif(neighbors(snapshot, row, col) < 4):
                 pass
             else:
+                print("die")
                 self.grid[row][col] = False
         else: # if dead
-            pass
+            if(neighbors(snapshot, row, col) == 3):
+                print("populate")
+                self.grid[row][col] = True
 
     def step(self):
-        snapshot = self.grid
+        snapshot = self.grid.copy()
 
         for i in range(0, len(self.grid)):
             for j in range(0, len(self.grid[i])):
                 self.act(snapshot, i, j)
 
+        print(f'steps; {self.steps}')
         self.steps += 1
     
+    def getgrid(self):
+        return self.grid
 
 
--- GameDisplay.py
+++ GameDisplay.py
@@ -1,16 +1,23 @@
 import gi
+import Game
 from random import randrange
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GObject
 
-class frame:
+class GameDisplay:
 
     def __init__(self):
         self.window = Gtk.Window(title="Conway's Game of Life")
         self.window.connect("destroy", Gtk.main_quit)
         self.window.set_default_size(450, 550)
         self.window.set_border_width(5)
+
+        width = 40
+        height = 40 
+
+        self.game = Game.Game(width, height)
+
 
         self.drawingarea = Gtk.DrawingArea()
         self.frame = Gtk.Frame()
@@ -21,21 +28,39 @@ class frame:
         self.window.show()
         self.frame.show()
         self.drawingarea.show()
-        GObject.timeout_add(100, self.refresh) # Go call tick every 50 whatsits.
+        GObject.timeout_add(100, self.update) 
 
     def draw(self, da, ctx):
-        ctx.set_source_rgb(1, 0, 0)
-        ctx.rectangle(randrange(1, 400), 50, 20, 120)
-        ctx.fill()
+        self.game.step()
+
+        bwidth, bheight = (8, 8)
+
+        grid = self.game.getgrid()
+
+        # I hate python lists so much
+        for row in range(0, len(grid)):
+            for col in range(0, len(grid[row])):
+                ctx.rectangle(row*bwidth, col*bheight, bwidth, bheight)
+                if(grid[row][col]):
+                    ctx.set_source_rgb(.9, .9, .9)
+                    ctx.fill_preserve()
+                    ctx.set_source_rgb(0.6, 0.6, 0.6)
+                    ctx.set_line_width(2)
+                    ctx.stroke()
+                else:
+                    ctx.set_source_rgb(0.6, 0.6, 0.6)
+                    ctx.set_line_width(2)
+                    ctx.stroke()
+
         return True
 
-    def refresh(self):
+    def update(self):
         self.drawingarea.queue_draw()
         return True
 
     def main(self):
         Gtk.main()
 
-b = frame()
-b.main()
+run = GameDisplay()
+run.main()
 
